4d40998dc3b2beef6980201963983856dd51e495
Bruce Pinn <brucepinn@gmail.com>
2021-03-17 19:21:51 -0400

    Created solution to DogYears exercise

---
+++ code_samples/007-documentation/DogYears/dog/main.go
@@ -0,0 +1,7 @@
+//Package dog provides year calculations for dogs
+package dog
+
+//Years takes number of human years and converts it to dog years.
+func Years(h int) int {
+	return 7 * h
+}
---
+++ code_samples/007-documentation/DogYears/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bwpinn/go-programming/code_samples/008-ninja-level-twelve/01/dog"
+)
+
+func main() {
+	dogyears := dog.Years(7)
+	fmt.Println(dogyears)
+}
